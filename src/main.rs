ab066df359c8d69e5aad8a15229e3aa68f68ae9c
Tobias Grothmann <tobiasgrothmannplotterart@gmail.com>
2024-06-19 20:00:14 +0200

    compute without precomputed primes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-use std::collections::HashSet;
+use primal::is_prime;
+use rayon::prelude::*;
+use simple_tqdm::ParTqdm;
 
-use primal::Primes;
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
-use thousands::Separable;
-
-fn caboose_fraction(c: u64, primes: &HashSet<u64>) -> f64 {
+fn caboose_fraction(c: u64) -> f64 {
     let mut count = 0_u64;
     for n in 1..(c - 1) {
         let number: u64 = n.pow(2) - n + c;
-        let prime = primes.contains(&number);
+        let prime = is_prime(number);
         if prime {
             count += 1;
         }
@@ -18,20 +16,12 @@ fn caboose_fraction(c: u64, primes: &HashSet<u64>) -> f64 {
 
 fn main() {
     let target: u64 = 1_000_000;
-    println!(
-        "generating primes up to {}^2 = {}",
-        target.separate_with_commas(),
-        target.pow(2).separate_with_commas()
-    );
-    let primes = Primes::all()
-        .take_while(|&p| p as u64 <= target.pow(2))
-        .map(|p| p as u64)
-        .collect::<HashSet<u64>>();
 
     println!("checking caboose numbers");
-    let caboose_fractions: Vec<(u64, f64)> = (3..target)
+    let caboose_fractions: Vec<(usize, f64)> = (3..target as usize)
         .into_par_iter()
-        .map(|c| (c, caboose_fraction(c, &primes)))
+        .tqdm()
+        .map(|c| (c, caboose_fraction(c as u64)))
         .collect();
 
     println!("writing to caboose_fractions.csv");
